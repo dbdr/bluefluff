f2389a672a4410008ed2e29f1de1d016e25babeb
Daniel Bonniot <dbonniot@chemaxon.com>
2017-04-07 10:48:30 -0700

    Print main entries

--- util/unpack.py
+++ util/unpack.py
@@ -19,7 +19,25 @@ def printBuf(buf):
 	for byte in buf:
 		hexLine += format(byte, '02x') + ' '
 	print(hexLine)
-	
+
+def parseEntry(dlc):
+	entryHeader = dlc.read(18)
+	if entryHeader[0] == 0xff and entryHeader[1] == 0x7f:
+		return False
+	if entryHeader[0] == 0:
+		# skip the rest of the (empty) entry
+		dlc.read(20)
+		return True
+	if entryHeader != b'D\x00L\x00C\x00_\x000\x000\x000\x000\x00.\x00':
+		print('Surprising entry header %s' % entryHeader)
+		exit(1)
+	entryType = dlc.read(8)
+	type = '%c%c%c' % (entryType[0],entryType[2],entryType[4])
+	print(type)
+	entry = dlc.read(12)
+	printBuf(entry)
+	return True
+
 with open(args.dlcfile, "rb") as dlc:
 	magic = dlc.read(10)
 
@@ -29,4 +47,7 @@ with open(args.dlcfile, "rb") as dlc:
 	
 	header = dlc.read(68)
 
-	printBuf(header)
+	#printBuf(header)
+
+	while parseEntry(dlc):
+		pass
